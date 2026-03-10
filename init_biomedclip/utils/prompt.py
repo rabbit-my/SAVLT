ad63eb3f547c579b8434879089fffb3875a48112
Yinmi <93649688+rabbit-my@users.noreply.github.com>
2026-03-10 15:29:24 +0800

    Create prompt.py

---
+++ init_biomedclip/utils/prompt.py
@@ -0,0 +1,43 @@
+
+# descriptions = {
+#     "mild_inflammation": 
+#         'Medical OCT image showing distinct layered structures with gray-white upper and lower layers',
+#     "cyst": 
+#         'Medical OCT image with smooth curved layered structures and oval dark areas',
+#     "ectropion": 
+#         'Medical OCT image displaying continuous papillary protrusions from columnar epithelium in upper region',
+#     "high-grade_squamous_intraepithelial_lesion": 
+#         'Medical OCT image showing densely packed icicle-like structures',
+#     "cancer": 
+#         'Medical OCT image revealing irregular tumor nests and necrotic dark areas'
+# }
+
+# prompt A
+
+# descriptions = {
+#     "mild_inflammation": 
+#         "a photo of a cervical tissue with mild inflammation",
+#     "cyst": 
+#         "a photo of a cervical tissue with a cyst",
+#     "ectropion": 
+#         "a photo of a cervical tissue with ectropion",
+#     "high-grade_squamous_intraepithelial_lesion": 
+#         "a photo of a cervical tissue with a high-grade squamous intraepithelial lesion",
+#     "cancer": 
+#         "a photo of a cervical tissue with cancer"
+# }
+
+# prompt B
+
+descriptions = {
+    "mild_inflammation": 
+        'A scan of inflamed cervical tissue showing a textured surface layer, and deeper layers with a clear dividing line leading into the stroma.',
+    "cyst": 
+        'A cervical tissue scan shows a clearly textured surface layer. In the middle or lower part of the image, a distinct dark elliptical or curved region is visible, which is characteristic of a cyst.',
+    "ectropion": 
+        'A cervical tissue scan featuring distinct clusters of protruding structures, representing the hallmark appearance of columnar epithelium eversion.',
+    "high-grade_squamous_intraepithelial_lesion": 
+        'The image features alternating light and dark vertical streaks resembling icy peak formations, indicating significant structural disruption.',
+    "cancer": 
+        'The image contains irregular structures and potential clusters resembling cancer nests, with significant disruption in the normal tissue architecture.'
+}
